75d826cbfa6b626f7089c64c75db209de3c5edf7
Gabe Wyatt <gabe@solarixdigital.com>
2020-04-22 20:01:50 -0700

    Fixed issue causing Users without any assigned Roles to be granted full Admin access (closes #48).   Base SaaS 'hasRole' method logic has two noticeable mistakes that cause this issue.    Users without ANY roles were not being automatically short circuited / discarded.    Iterative loop when validating for Roles with children would return true after the *first* successful iteration,     thereby skipping all subsequent iterations.

--- app/App/Traits/Eloquent/Roles/HasRoles.php
+++ app/App/Traits/Eloquent/Roles/HasRoles.php
@@ -12,119 +12,129 @@ use Carbon\Carbon;
 use CreatyDev\Domain\Users\Models\Role;
 use CreatyDev\Domain\Users\Models\UserRole;
 
-trait HasRoles
-{
-    /**
-     * Assign user a role by id.
-     *
-     * @param $roleId
-     * @param null $expiresAt
-     */
-    public function assignRoleById($roleId, $expiresAt = null)
-    {
-        $role = Role::where('id', $roleId)->first();
+trait HasRoles {
+  /**
+   * Assign user a role by id.
+   *
+   * @param $roleId
+   * @param null $expiresAt
+   */
+  public function assignRoleById($roleId, $expiresAt = null) {
+    $role = Role::where('id', $roleId)->first();
 
-        $this->assignRole($role, $expiresAt);
+    $this->assignRole($role, $expiresAt);
+  }
+
+  /**
+   * Assign user a role.
+   * Returns true if role added.
+   *
+   * @param Role $role
+   * @param null $expiresAt
+   * @return bool
+   */
+  public function assignRole(Role $role, $expiresAt = null) {
+    if (!$this->hasRole($role->slug)) {
+      if (isset($expiresAt)) {
+        $expiresAt = Carbon::parse($expiresAt)->toDateTimeString();
+      }
+
+      $this->roles()->attach($role->id, ['expires_at' => $expiresAt]);
+
+      return true;
     }
 
-    /**
-     * Assign user a role.
-     * Returns true if role added.
-     *
-     * @param Role $role
-     * @param null $expiresAt
-     * @return bool
-     */
-    public function assignRole(Role $role, $expiresAt = null)
-    {
-        if (!($this->hasRole($role->slug))) {
+    return false;
+  }
 
-            if (isset($expiresAt)) {
-                $expiresAt = Carbon::parse($expiresAt)->toDateTimeString();
-            }
+  /**
+   * Update user's role by id.
+   *
+   * @param $roleId
+   * @param null $expiresAt
+   */
+  public function updateRoleById($roleId, $expiresAt = null) {
+    $role = Role::where('id', $roleId)->first();
 
-            $this->roles()->attach($role->id, ['expires_at' => $expiresAt]);
+    $this->updateRole($role, $expiresAt);
+  }
 
-            return true;
-        }
+  /**
+   * Update user's role.
+   *
+   * @param Role $role
+   * @param null $expiresAt
+   * @return bool
+   */
+  public function updateRole(Role $role, $expiresAt = null) {
+    if ($this->hasRole($role->slug)) {
+      if (isset($expiresAt)) {
+        $expiresAt = Carbon::parse($expiresAt)->toDateTimeString();
+      }
 
-        return false;
+      $this->roles()->updateExistingPivot($role->id, [
+        'expires_at' => $expiresAt
+      ]);
+
+      return true;
     }
 
-    /**
-     * Update user's role by id.
-     *
-     * @param $roleId
-     * @param null $expiresAt
-     */
-    public function updateRoleById($roleId, $expiresAt = null)
-    {
-        $role = Role::where('id', $roleId)->first();
+    return false;
+  }
 
-        $this->updateRole($role, $expiresAt);
+  /**
+   * Check if given model has role.
+   *
+   * if they do not have given role or its child
+   * then return false
+   *
+   * Uses nested sets for parent/child relationships.
+   * @see https://github.com/lazychaser/laravel-nestedset
+   *
+   * @param $roles
+   * @return bool
+   */
+  public function hasRole(...$roles) {
+    $userRoles = $this->roles();
+
+    // User has no roles
+    if (!$userRoles) {
+      return false;
     }
 
-    /**
-     * Update user's role.
-     *
-     * @param Role $role
-     * @param null $expiresAt
-     * @return bool
-     */
-    public function updateRole(Role $role, $expiresAt = null)
-    {
-        if ($this->hasRole($role->slug)) {
-            if (isset($expiresAt)) {
-                $expiresAt = Carbon::parse($expiresAt)->toDateTimeString();
-            }
+    // Get passed role with children
+    $roles = Role::with('children')
+      ->whereIn('slug', $roles)
+      ->get();
 
-            $this->roles()->updateExistingPivot($role->id, ['expires_at' => $expiresAt]);
-
-            return true;
-        }
-
-        return false;
+    if (!$roles) {
+      // Short circuit due to assumed invalid role.
+      return false;
     }
 
-    /**
-     * Check if given model has role.
-     *
-     * if they do not have given role or its child
-     * then return false
-     *
-     * @param $roles
-     * @return bool
-     */
-    public function hasRole(...$roles)
-    {
-        $roles = Role::with('children')->whereIn('slug', $roles)->get();
+    // Iterate each role
+    foreach ($roles as $role) {
+      // Fetch child roles as string array
+      $children = $role->children->pluck('slug')->push($role->slug);
 
-        foreach ($roles as $role) {
-
-            //fetch role's children
-            $children = $role->children->pluck('slug')->push($role->slug);
-
-            if (!($this->roles()->WhereNull('expires_at')
-                ->orWhereDate('expires_at', '>', Carbon::now())
-                ->whereIn('slug', $children)->count())) {
-                return false;
-            }
-
-            return true;
-        }
-
-        return false;
+      // Check if User roles' slug is found within child roles array
+      if (!!$userRoles->whereIn('slug', $children)->count()) {
+        return true;
+      }
     }
 
-    /**
-     * Get the roles that belong to the model.
-     */
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class, 'user_roles')
-            ->using(UserRole::class)
-            ->as('roleable')
-            ->withTimestamps()
-            ->withPivot(['expires_at']);
-    }
+    // No role check succeeded, so implicitly fail.
+    return false;
+  }
+
+  /**
+   * Get the roles that belong to the model.
+   */
+  public function roles() {
+    return $this->belongsToMany(Role::class, 'user_roles')
+      ->using(UserRole::class)
+      ->as('roleable')
+      ->withTimestamps()
+      ->withPivot(['expires_at']);
+  }
 }
