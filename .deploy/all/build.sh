d3459499cbb181bd555e3962c3aa7aa0c3d2671d
Gabe Wyatt <gabe@solarixdigital.com>
2020-05-04 23:45:44 -0700

    Debugging build script ENV_FILE variable not displaying.

--- .deploy/all/build.sh
+++ .deploy/all/build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+echo "Outer SSH_PRIVATE_KEY: ${SSH_PRIVATE_KEY}"
+echo "Outer ENV_FILE: ${ENV_FILE}"
+
 # Update repo to build-flag, pull, install, and build app.
 ssh -o StrictHostKeyChecking=no ubuntu@"${DEPLOY_ENDPOINT}" << EOF
+  echo "Inner SSH_PRIVATE_KEY: ${SSH_PRIVATE_KEY}"
+  echo "Inner ENV_FILE: ${ENV_FILE}"
+  echo "Inner SSH_PRIVATE_KEY escaped: \${SSH_PRIVATE_KEY}"
+  echo "Inner ENV_FILE escaped: \${ENV_FILE}"
   cd ${TARGET_DIRECTORY}
   echo "Setting remote origin url to '${REPOSITORY_URL}'."
   git remote set-url origin ${REPOSITORY_URL}
