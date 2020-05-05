7d05de13601b4eadaf61bbd6e69eca23f02e0b67
Gabe Wyatt <gabe@solarixdigital.com>
2020-05-05 00:23:57 -0700

    Moving .env update to SCP transfer, as the file path provided by GitLab is on local docker instance, not remote.

--- .deploy/all/build.sh
+++ .deploy/all/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Update repo to build-flag, pull, install, and build app.
+# Update repo to build-flag, pull, and checkout.
 ssh -o StrictHostKeyChecking=no ubuntu@"${DEPLOY_ENDPOINT}" << EOF
   cd ${TARGET_DIRECTORY}
   echo "Setting remote origin url to '${REPOSITORY_URL}'."
@@ -9,8 +9,15 @@ ssh -o StrictHostKeyChecking=no ubuntu@"${DEPLOY_ENDPOINT}" << EOF
   echo "Pulling and checking out '${CI_COMMIT_REF_NAME}' branch."
   git pull origin ${CI_COMMIT_REF_NAME}
   git checkout ${CI_COMMIT_REF_NAME}
-  echo "Copying from ${ENV_FILE} to .env."
-  cp ${ENV_FILE} .env
+EOF
+
+# Transfer .env file
+echo "Copying ${ENV_FILE} to .env."
+scp -o StrictHostKeyChecking=no "${ENV_FILE}" "ubuntu@${DEPLOY_ENDPOINT}:${TARGET_DIRECTORY}/.env"
+
+# Installing packages and building app.
+ssh -o StrictHostKeyChecking=no ubuntu@"${DEPLOY_ENDPOINT}" << EOF
+  cd ${TARGET_DIRECTORY}
   echo "Installing composer packages."
   composer install
   echo "Installing node modules."
