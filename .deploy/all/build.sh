ca54ca30bcd0817e8e3524d663331687dcd5596f
Gabe Wyatt <gabe@solarixdigital.com>
2020-05-05 00:14:18 -0700

    Moving ENV_FILE variable definition into build script as defined export.   This is related to a 3.5+ year-old issue (implementation plan revised 01/22/20), as GitLab CI/CD does not properly    handle file path-based environment secondary reassignment within gitlab.ci.yml.  Current workaround seems to be to    explicitly define variable export as normal bash script line.   See: https://gitlab.com/gitlab-org/gitlab-runner/-/issues/1809

--- .deploy/all/build.sh
+++ .deploy/all/build.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-echo "Outer ENV_FILE_TESTING: ${ENV_FILE_TESTING}"
-echo "Outer SSH_PRIVATE_KEY: ${SSH_PRIVATE_KEY}"
-echo "Outer ENV_FILE: ${ENV_FILE}"
-
 # Update repo to build-flag, pull, install, and build app.
 ssh -o StrictHostKeyChecking=no ubuntu@"${DEPLOY_ENDPOINT}" << EOF
-  echo "Inner ENV_FILE_TESTING: ${ENV_FILE_TESTING}"
-  echo "Inner SSH_PRIVATE_KEY: ${SSH_PRIVATE_KEY}"
-  echo "Inner ENV_FILE: ${ENV_FILE}"
-  echo "Inner SSH_PRIVATE_KEY escaped: \${SSH_PRIVATE_KEY}"
-  echo "Inner ENV_FILE escaped: \${ENV_FILE}"
   cd ${TARGET_DIRECTORY}
   echo "Setting remote origin url to '${REPOSITORY_URL}'."
   git remote set-url origin ${REPOSITORY_URL}
