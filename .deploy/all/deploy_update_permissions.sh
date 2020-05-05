7260cda0cf40980d9588cc562944c1639508fd40
Gabe Wyatt <gabe@solarixdigital.com>
2020-05-04 18:11:14 -0700

    Escaped inline variables in deploy/update_permissions script.

--- .deploy/all/deploy_update_permissions.sh
+++ .deploy/all/deploy_update_permissions.sh
@@ -7,15 +7,15 @@ ssh -o StrictHostKeyChecking=no ubuntu@"${DEPLOY_ENDPOINT}" << EOF
   LARAVEL_WS_GROUP=www-data
   LARAVEL_ROOT=${TARGET_DIRECTORY}
 
-  sudo usermod -a -G "${LARAVEL_WS_GROUP}" "${LARAVEL_OWNER}"
+  sudo usermod -a -G "\${LARAVEL_WS_GROUP}" "\${LARAVEL_OWNER}"
 
-  sudo chown -R "${LARAVEL_OWNER}:${LARAVEL_WS_GROUP}" "${LARAVEL_ROOT}"
+  sudo chown -R "\${LARAVEL_OWNER}:\${LARAVEL_WS_GROUP}" "\${LARAVEL_ROOT}"
 
-  sudo find "${LARAVEL_ROOT}" -type f -exec chmod 644 {} \;
+  sudo find "\${LARAVEL_ROOT}" -type f -exec chmod 644 {} \;
 
-  sudo find "${LARAVEL_ROOT}" -type d -exec chmod 755 {} \;
+  sudo find "\${LARAVEL_ROOT}" -type d -exec chmod 755 {} \;
 
-  sudo chgrp -R "${LARAVEL_WS_GROUP}" "${LARAVEL_ROOT}/storage" "${LARAVEL_ROOT}/bootstrap/cache"
+  sudo chgrp -R "\${LARAVEL_WS_GROUP}" "\${LARAVEL_ROOT}/storage" "\${LARAVEL_ROOT}/bootstrap/cache"
 
-  sudo chmod -R ug+rwx "${LARAVEL_ROOT}/storage" "${LARAVEL_ROOT}/bootstrap/cache"
+  sudo chmod -R ug+rwx "\${LARAVEL_ROOT}/storage" "\${LARAVEL_ROOT}/bootstrap/cache"
 EOF
