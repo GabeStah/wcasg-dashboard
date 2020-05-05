13e4fd553171e358880d650db3ea68b39dc422e8
Gabe Wyatt <gabe@solarixdigital.com>
2020-05-04 23:35:26 -0700

    ENV_FILE not picked up in gitlab-ci.yml.  Trying quotation escapage.

--- .deploy/all/build.sh
+++ .deploy/all/build.sh
@@ -3,13 +3,19 @@
 # Update repo to build-flag, pull, install, and build app.
 ssh -o StrictHostKeyChecking=no ubuntu@"${DEPLOY_ENDPOINT}" << EOF
   cd ${TARGET_DIRECTORY}
+  echo "Setting remote origin url to '${REPOSITORY_URL}'."
   git remote set-url origin ${REPOSITORY_URL}
   git stash
+  echo "Pulling and checking out '${CI_COMMIT_REF_NAME}' branch."
   git pull origin ${CI_COMMIT_REF_NAME}
   git checkout ${CI_COMMIT_REF_NAME}
+  echo "Copying from ${ENV_FILE} to .env."
   cp ${ENV_FILE} .env
+  echo "Installing composer packages."
   composer install
+  echo "Installing node modules."
   yarn install
   npm rebuild
+  echo "Building app."
   yarn run ${CI_ENVIRONMENT_NAME}
 EOF
