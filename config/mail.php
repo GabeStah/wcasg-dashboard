b9a3266252073578f09ea3be36b7d4b05c17bdc8
Gabe Wyatt <gabe@solarixdigital.com>
2020-05-15 07:35:27 -0700

    ci(Production): Update env credentials for Stripe, Intuit, etc to live versions
    Most changes are hidden from repo and defined in CI/CD ENV variables.
    
    Refs https://gitlab.solarixdigital.com/solarix/wcasg/dashboard/-/issues/67

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     */
 
   'from' => [
-    'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+    'address' => env('MAIL_FROM_ADDRESS', 'support@wcasg.com'),
     'name' => env('MAIL_FROM_NAME', 'Example')
   ],
 
