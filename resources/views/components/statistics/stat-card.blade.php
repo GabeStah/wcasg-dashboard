fa035753249402c3f98f8fa1d4515a1b2edbc8ed
Gabe Wyatt <gabe@solarixdigital.com>
2020-10-06 09:00:47 -0700

    style: shrink statistic value font size to reduce wrapping

--- resources/views/components/statistics/stat-card.blade.php
+++ resources/views/components/statistics/stat-card.blade.php
@@ -1,6 +1,6 @@
 <div class="card bg-info border-0 centered">
   <div class="card-body">
-    <h3 class="display-2 text-center text-white">{{ $value }}</h3>
+    <h3 class="display-3 text-center text-white">{{ $value }}</h3>
     @isset($period)
       <p class="text-center mb-0">{{ $period }}</p>
     @endisset
