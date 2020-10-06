6354c4413b28a6a672f3fe3219506112308072c5
Gabe Wyatt <gabe@solarixdigital.com>
2020-10-06 08:39:18 -0700

    refactor: remove previous month, year, and total statistics rows from Dashboard
    Refs #75
    
    docs: add production ip to Coeus WCASG statistics User example

--- resources/views/account/dashboard/index.blade.php
+++ resources/views/account/dashboard/index.blade.php
@@ -107,84 +107,84 @@
                 @endcomponent
               </div>
             </div>
-            <!-- Previous Month -->
-            <div class="row">
-              <div class="col-4">
-                @component('components.statistics.stat-card', [
-                  'value' => number_format($statistics['views']['month']['previous']),
-                  'period' => (new DateTime())->modify('-1 month')->format('F Y'),
-                  'title' => 'Views'
-                ])
-                @endcomponent
-              </div>
-              <div class="col-4">
-                @component('components.statistics.stat-card', [
-                  'value' => format_bytes($statistics['bytes']['month']['previous']),
-                  'period' => (new DateTime())->modify('-1 month')->format('F Y'),
-                  'title' => 'Bandwidth'
-                ])
-                @endcomponent
-              </div>
-              <div class="col-4">
-                @component('components.statistics.stat-card', [
-                  'value' => format_bytes($statistics['bytes']['tts']['month']['previous']),
-                  'period' => (new DateTime())->modify('-1 month')->format('F Y'),
-                  'title' => 'TTS Bandwidth'
-                ])
-                @endcomponent
-              </div>
-            </div>
-            <!-- Year -->
-            <div class="row">
-              <div class="col-4">
-                @component('components.statistics.stat-card', [
-                  'value' => number_format($statistics['views']['year']),
-                  'period' => (new DateTime())->format('Y'),
-                  'title' => 'Views'
-                ])
-                @endcomponent
-              </div>
-              <div class="col-4">
-                @component('components.statistics.stat-card', [
-                  'value' => format_bytes($statistics['bytes']['year']),
-                  'period' => (new DateTime())->format('Y'),
-                  'title' => 'Bandwidth'
-                ])
-                @endcomponent
-              </div>
-              <div class="col-4">
-                @component('components.statistics.stat-card', [
-                  'value' => format_bytes($statistics['bytes']['tts']['year']),
-                  'period' => (new DateTime())->format('Y'),
-                  'title' => 'TTS Bandwidth'
-                ])
-                @endcomponent
-              </div>
-            </div>
-            <!-- Total -->
-            <div class="row">
-              <div class="col-4">
-                @component('components.statistics.stat-card', [
-                  'value' => number_format($statistics['views']['total']),
-                  'title' => 'Total Views'
-                ])
-                @endcomponent
-              </div>
-              <div class="col-4">
-                @component('components.statistics.stat-card', [
-                  'value' => format_bytes($statistics['bytes']['total']),
-                  'title' => 'Total Bandwidth'
-                ])
-                @endcomponent
-              </div>
-              <div class="col-4">
-                @component('components.statistics.stat-card', [
-                  'value' => format_bytes($statistics['bytes']['tts']['total']),
-                  'title' => 'Total TTS Bandwidth'
-                ])
-                @endcomponent
-              </div>
-            </div>
+{{--            <!-- Previous Month -->--}}
+{{--            <div class="row">--}}
+{{--              <div class="col-4">--}}
+{{--                @component('components.statistics.stat-card', [--}}
+{{--                  'value' => number_format($statistics['views']['month']['previous']),--}}
+{{--                  'period' => (new DateTime())->modify('-1 month')->format('F Y'),--}}
+{{--                  'title' => 'Views'--}}
+{{--                ])--}}
+{{--                @endcomponent--}}
+{{--              </div>--}}
+{{--              <div class="col-4">--}}
+{{--                @component('components.statistics.stat-card', [--}}
+{{--                  'value' => format_bytes($statistics['bytes']['month']['previous']),--}}
+{{--                  'period' => (new DateTime())->modify('-1 month')->format('F Y'),--}}
+{{--                  'title' => 'Bandwidth'--}}
+{{--                ])--}}
+{{--                @endcomponent--}}
+{{--              </div>--}}
+{{--              <div class="col-4">--}}
+{{--                @component('components.statistics.stat-card', [--}}
+{{--                  'value' => format_bytes($statistics['bytes']['tts']['month']['previous']),--}}
+{{--                  'period' => (new DateTime())->modify('-1 month')->format('F Y'),--}}
+{{--                  'title' => 'TTS Bandwidth'--}}
+{{--                ])--}}
+{{--                @endcomponent--}}
+{{--              </div>--}}
+{{--            </div>--}}
+{{--            <!-- Year -->--}}
+{{--            <div class="row">--}}
+{{--              <div class="col-4">--}}
+{{--                @component('components.statistics.stat-card', [--}}
+{{--                  'value' => number_format($statistics['views']['year']),--}}
+{{--                  'period' => (new DateTime())->format('Y'),--}}
+{{--                  'title' => 'Views'--}}
+{{--                ])--}}
+{{--                @endcomponent--}}
+{{--              </div>--}}
+{{--              <div class="col-4">--}}
+{{--                @component('components.statistics.stat-card', [--}}
+{{--                  'value' => format_bytes($statistics['bytes']['year']),--}}
+{{--                  'period' => (new DateTime())->format('Y'),--}}
+{{--                  'title' => 'Bandwidth'--}}
+{{--                ])--}}
+{{--                @endcomponent--}}
+{{--              </div>--}}
+{{--              <div class="col-4">--}}
+{{--                @component('components.statistics.stat-card', [--}}
+{{--                  'value' => format_bytes($statistics['bytes']['tts']['year']),--}}
+{{--                  'period' => (new DateTime())->format('Y'),--}}
+{{--                  'title' => 'TTS Bandwidth'--}}
+{{--                ])--}}
+{{--                @endcomponent--}}
+{{--              </div>--}}
+{{--            </div>--}}
+{{--            <!-- Total -->--}}
+{{--            <div class="row">--}}
+{{--              <div class="col-4">--}}
+{{--                @component('components.statistics.stat-card', [--}}
+{{--                  'value' => number_format($statistics['views']['total']),--}}
+{{--                  'title' => 'Total Views'--}}
+{{--                ])--}}
+{{--                @endcomponent--}}
+{{--              </div>--}}
+{{--              <div class="col-4">--}}
+{{--                @component('components.statistics.stat-card', [--}}
+{{--                  'value' => format_bytes($statistics['bytes']['total']),--}}
+{{--                  'title' => 'Total Bandwidth'--}}
+{{--                ])--}}
+{{--                @endcomponent--}}
+{{--              </div>--}}
+{{--              <div class="col-4">--}}
+{{--                @component('components.statistics.stat-card', [--}}
+{{--                  'value' => format_bytes($statistics['bytes']['tts']['total']),--}}
+{{--                  'title' => 'Total TTS Bandwidth'--}}
+{{--                ])--}}
+{{--                @endcomponent--}}
+{{--              </div>--}}
+{{--            </div>--}}
           @endisset
       </div>
   </div>
